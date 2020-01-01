7a05c79000c331f9a2de52906c54eee1eefb541b
Abbas <abbazs@gmail.com>
2020-01-01 21:49:55 +0530

    Added V7 files to V5

--- run_current.sh
+++ run_current.sh
@@ -1,12 +1,21 @@
 # python run.py -mitr=3 -ssaf=0.95 -noad=1 ssrc -name=nov_ssrc4.json
 # python run.py -mitr=3 -ssaf=0.95 -noad=1 ssrc -name=nov_ssrc5.json
 # python run.py -mitr=10 -ssaf=0.95 -noad=1 ssrc -name=dec_ssrc1.json
-python run.py -mitr=10 -ssaf=0.95 -noad=1 ssrc -name=dec_ssrc2.json
+# python run.py -mitr=10 -ssaf=0.95 -noad=1 ssrc -name=dec_ssrc2.json
 # python run.py -mitr=10 -ssaf=0.95 -noad=1 ssrc -name=dec_ssrc3.json
 # python run.py -mitr=10 -ssaf=0.95 -noad=1 ssrc -name=dec_ssrc4.json
-python run.py -mitr=10 -ssaf=0.95 -noad=1 ssrc -name=dec_ssrc5.json
+# python run.py -mitr=10 -ssaf=0.95 -noad=1 ssrc -name=dec_ssrc5.json
+python run.py -mitr=0 -ssaf=0.95 -noad=1 ssgcf -name=jan_ssgcf1.json
 python run.py -mitr=10 -ssaf=0.95 -noad=1 ssrc -name=jan_ssrc1.json
 python run.py -mitr=10 -ssaf=0.95 -noad=1 ssrc -name=jan_ssrc2.json
 python run.py -mitr=10 -ssaf=0.95 -noad=1 ssrc -name=jan_ssrc3.json
+python run.py -mitr=10 -ssaf=0.95 -noad=1 ssrc -name=jan_ssrc4.json
+python run.py -mitr=10 -ssaf=0.95 -noad=1 ssrc -name=jan_ssrc5.json
+python run.py -mitr=10 -ssaf=0.95 -noad=1 ssrc -name=jan_ssrc6.json
 python run.py -mitr=10 -ssaf=0.95 -noad=1 ssrc -name=feb_ssrc1.json
-
+python run.py -mitr=10 -ssaf=0.95 -noad=1 ssrc -name=feb_ssrc2.json
+python run.py -mitr=10 -ssaf=0.95 -noad=1 ssgcf -name=mar_ssgcf1.json
+python run.py -mitr=10 -ssaf=0.95 -noad=1 ssrc -name=mar_ssrc1.json
+# python run.py -symbol=NBCC -mitr=10 -ssaf=0.95 -noad=1 ssrc -name=nbcc_jan30_1.json
+# python run.py -symbol=NIFTY -ssaf=0.95 -noad=1 -sincr=50 ssrs -ST='24-dec-2019' -ND='26-mar-2020' -ED='26-mar-2020'
+# python run.py -symbol=NBCC -ssaf=0.95 -noad=1 -sincr=2 ssrs -ST='24-dec-2019' -ND='30-JAN-2020' -ED='30-JAN-2020'
